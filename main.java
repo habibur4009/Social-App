52ffe0c9e2c72f694603a29f22758b0cf6718ebf
habibur4009 <mdhabibur4009@gmail.com>
2024-10-23 19:08:00 +0600

    First commit from Habibur Rahman

---
+++ main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class main {
+
+    public static void main(String[] args) {
+        String userrName;
+        String password;
+        int age;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter user Name:");
+        userrName = scanner.nextLine();
+
+        System.out.println("Enter password:");
+        password = scanner.nextLine();
+
+        System.out.println("Enter your age:");
+        age = scanner.nextInt();
+
+    }
+}
